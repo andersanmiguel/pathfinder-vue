c006a4b703e19eacc57ea0674ce10f480ec52adb
Ander San Miguel <andersanmiguel@gmail.com>
2017-10-04 20:20:49 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div class="timer" v-if="timer > 0">{{ timer }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      timer: 5,
+      count: ''
+    };
+  },
+  watch: {
+    timer: function() {
+      if (this.timer === 0) {
+        clearInterval(this.count);
+      }
+    }
+  },
+  mounted: function() {
+    this.countDown();
+  },
+  methods: {
+    countDown: function() {
+      this.count = setInterval(() => {
+        this.timer--;
+      }, 1000);
+    }
+  }
+}
+</script>

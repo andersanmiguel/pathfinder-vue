54181340bc28269740e23a07b32bf85b5a8434fb
Ander San Miguel <andersanmiguel@gmail.com>
2017-10-25 22:29:45 +0200

    Paint the hint

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,7 @@ export default {
       initial_timer: 5,
       level: 2,
       initial_grid: 3,
+      level_values: [],
       board: []
     };
   },
@@ -30,6 +31,7 @@ export default {
   },
   mounted: function() {
     this.make_board();
+    this.prepareLevel();
   },
   methods: {
     make_board: function() {
@@ -42,6 +44,66 @@ export default {
     },
     checkTile: function(tile_index) {
       console.log(tile_index);
+    },
+
+
+    prepareLevel: function() {
+      this.level_values = [];
+      let row = this.grid;
+      let current = game.getRandomInt(this.grid * this.grid - this.grid + 1, this.grid * this.grid);
+      this.level_values.push(current);
+      this.nextValid(current, row);
+      this.paintHint();
+    },
+
+    paintHint: function() {
+      this.level_values.forEach((item) => {
+        this.board[item - 1].hint = true;
+      });
+    },
+
+    nextValid: function(current, row) {
+      let row_values = this.row_values(row);
+      if (row === 1 && row_values.includes(current)) {
+        return 1;
+      } else {
+
+        let posibilities = [];
+
+        if (row_values.includes(current - 1)) {
+          posibilities.push(current - 1);
+        }
+
+        if (row_values.includes(current + 1)) {
+          posibilities.push(current + 1);
+        }
+
+        posibilities.push(current - this.grid);
+
+        let filtered = posibilities.filter((item) => {
+          return !this.level_values.includes(item);
+        });
+
+        let next_index = game.getRandomInt(0, filtered.length - 1);
+
+        let next = filtered[next_index];
+
+        this.level_values.push(next);
+
+        if (!row_values.includes(next) && row > 1) {
+          row--;
+        }
+
+        return this.nextValid(next, row);
+
+      }
+    },
+    row_values: function(row) {
+      let values = [];
+      for (let i = 1; i <= this.grid; i++) {
+        values.push(row * this.grid - i + 1);
+      }
+      return values;
     },
     end_count: function() {
       // Timer end
--- src/components/Tile.vue
+++ src/components/Tile.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="['tile', {hint: tile.hint, played: tile.played, error: tile.error}]" v-on:click="setActive"> {{ tile.key }} </div>
+  <div :class="['tile', {'hint': tile.hint, played: tile.played, error: tile.error}]" v-on:click="setActive"></div>
 </template>
 <script>
 export default {
